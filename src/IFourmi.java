8eec2e593d2ec9c251d7be3fac783cdf5fa3e388
flodavid <fl.david.53@gmail.com>
2016-04-06 17:40:40 +0200

    modif interface fourmi

--- src/IFourmi.java
+++ src/IFourmi.java
@@ -1,8 +1,11 @@
+import java.awt.Graphics;
+
 
 public interface IFourmi {
 	public void actionDepart(Case[][] c);
-	public void deplacement(Case[][] c);	//TODO grille est un tableau de cases
+	public void deplacement(Case[][] c,coordonnee maxGrille);	//TODO grille est un tableau de cases
 	public void actionArrive(Case[][] c);
-	
+	public boolean estMort();
+	public void paintComponent(Graphics g);
 
 }
